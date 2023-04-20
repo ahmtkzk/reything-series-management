975dc708b25aa8fe9cb7da01712f49fca44dc48b
Ahmet Kazak <ahmetkazak08@gmail.com>
2023-04-20 23:03:35 +0300

    Initial commit.

---
+++ src/main/java/com/reything/seriesmanagement/ReythingSeriesManagementApplication.java
@@ -0,0 +1,13 @@
+package com.reything.seriesmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReythingSeriesManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReythingSeriesManagementApplication.class, args);
+    }
+
+}
